6c2c8ecb13ed83514f3c3d18f0f4d35e8f8c27da
guzelokur <101527114+guzelokur@users.noreply.github.com>
2026-01-12 15:44:29 +0300

    Add files via upload

---
+++ javascript.js
@@ -0,0 +1,14 @@
+const cameraButton = document.getElementById('cameraButton');
+const videoElement = document.getElementById('videoElement');
+
+function openCamera() {
+  navigator.mediaDevices.getUserMedia({ video: true })
+    .then(function (stream) {
+    
+      videoElement.srcObject = stream;
+    })
+    .catch(function (error) {
+      console.log('Kamera erişimi reddedildi: ', error);
+    });
+}
+cameraButton.addEventListener('click', openCamera);
